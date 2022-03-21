41103db61f93aeebd48d09a896dea29729a0ca20
cxnt <pavelbartsits@gmail.com>
2022-03-21 18:55:27 +0300

    add tags from json upload meta

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -11,10 +11,20 @@ TEAM_ID: int = int(os.environ['context.teamId'])
 WORKSPACE_ID: int = int(os.environ['context.workspaceId'])
 
 TAG_MODE: str = os.environ["modal.state.tagMode"]
-GROUP_TAG_NAME: str = os.environ.get("modal.state.groupTag")
+
 UPLOAD_META: bool = bool(os.environ["modal.state.uploadMeta"])
 ADD_DCM_TAGS: bool = bool(os.environ["modal.state.addTagsFromDcm"])
 DCM_TAGS: list = json.loads(os.environ["modal.state.dcmTags"])["tags"]
+
+PREPARED_GROUP_TAG_NAME: str = os.environ.get("modal.state.preparedGroupTag")
+MANUAL_GROUP_TAG_NAME: str = os.environ.get("modal.state.manualGroupTag")
+
+GROUP_TAG_NAME = None
+if TAG_MODE == "prepared":
+    GROUP_TAG_NAME = PREPARED_GROUP_TAG_NAME
+else:
+    GROUP_TAG_NAME = MANUAL_GROUP_TAG_NAME
+
 
 INPUT_DIR: str = os.environ.get("modal.state.slyFolder")
 INPUT_FILE: str = os.environ.get("modal.state.slyFile")
