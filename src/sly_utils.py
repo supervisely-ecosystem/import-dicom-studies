2828da471e310ab8c6e416fb916dfba894e50227
cxnt <pavelbartsits@gmail.com>
2022-11-30 14:24:51 +0400

    fix nrrd names

--- src/sly_utils.py
+++ src/sly_utils.py
@@ -1,16 +1,21 @@
 import functools
-import nrrd
-import os
 import json
+import os
 import shutil
 from functools import partial
-from typing import Callable, List, Dict, Tuple
-import pydicom
-from pydicom import FileDataset
 from pathlib import Path
+from typing import Callable, Dict, List, Tuple
 
+import nrrd
+import pydicom
 import supervisely as sly
-from supervisely.io.fs import get_file_ext, get_file_name, get_file_name_with_ext, silent_remove
+from pydicom import FileDataset
+from supervisely.io.fs import (
+    get_file_ext,
+    get_file_name,
+    get_file_name_with_ext,
+    silent_remove,
+)
 
 import sly_globals as g
 
@@ -22,23 +27,18 @@ def update_progress(count, api: sly.Api, task_id: int, progress: sly.Progress) -
         progress.report_progress()
 
 
-def get_progress_cb(api: sly.Api, task_id: int, message: str, total: int, is_size: bool = False,
-                    func: Callable = update_progress) -> functools.partial:
+def get_progress_cb(
+    api: sly.Api,
+    task_id: int,
+    message: str,
+    total: int,
+    is_size: bool = False,
+    func: Callable = update_progress,
+) -> functools.partial:
     progress = sly.Progress(message, total, is_size=is_size)
     progress_cb = partial(func, api=api, task_id=task_id, progress=progress)
     progress_cb(0)
     return progress_cb
-
-
-def get_free_name(group_name: str, image_name: str) -> str:
-    """Generates new name for duplicated group image name."""
-    original_name = image_name
-    image_name, image_ext = get_file_name(image_name), get_file_ext(image_name)
-    res_name = '{}_{}{}'.format(
-        image_name, group_name, image_ext)
-    g.my_app.logger.warn(
-        f"Duplicated group image name found. Image: {original_name} has been renamed to {res_name}")
-    return res_name
 
 
 def is_dicom_file(path: str, verbose: bool = False) -> bool:
@@ -50,7 +50,8 @@ def is_dicom_file(path: str, verbose: bool = False) -> bool:
         if verbose:
             print("'{}' appears not to be a DICOM file\n({})".format(path, ex))
             g.my_app.logger.warn(
-                "'{}' appears not to be a DICOM file\n({})".format(path, ex))
+                "'{}' appears not to be a DICOM file\n({})".format(path, ex)
+            )
         result = False
     return result
 
@@ -61,55 +62,51 @@ def download_data_from_team_files(api: sly.Api, task_id: int, save_path: str) ->
     if g.INPUT_DIR is not None:
         remote_path = g.INPUT_DIR
         project_path = os.path.join(
-            save_path, os.path.basename(os.path.normpath(remote_path)))
+            save_path, os.path.basename(os.path.normpath(remote_path))
+        )
         sizeb = api.file.get_directory_size(g.TEAM_ID, remote_path)
-        progress_cb = get_progress_cb(api=api,
-                                      task_id=task_id,
-                                      message=f"Downloading {remote_path.lstrip('/').rstrip('/')}",
-                                      total=sizeb,
-                                      is_size=True)
-        api.file.download_directory(team_id=g.TEAM_ID,
-                                    remote_path=remote_path,
-                                    local_save_path=project_path,
-                                    progress_cb=progress_cb)
+        progress_cb = get_progress_cb(
+            api=api,
+            task_id=task_id,
+            message=f"Downloading {remote_path.lstrip('/').rstrip('/')}",
+            total=sizeb,
+            is_size=True,
+        )
+        api.file.download_directory(
+            team_id=g.TEAM_ID,
+            remote_path=remote_path,
+            local_save_path=project_path,
+            progress_cb=progress_cb,
+        )
 
     elif g.INPUT_FILE is not None:
         remote_path = g.INPUT_FILE
-        save_archive_path = os.path.join(
-            save_path, get_file_name_with_ext(remote_path))
+        save_archive_path = os.path.join(save_path, get_file_name_with_ext(remote_path))
         sizeb = api.file.get_info_by_path(g.TEAM_ID, remote_path).sizeb
-        progress_cb = get_progress_cb(api=api,
-                                      task_id=task_id,
-                                      message=f"Downloading {remote_path.lstrip('/')}",
-                                      total=sizeb,
-                                      is_size=True)
-        api.file.download(team_id=g.TEAM_ID,
-                          remote_path=remote_path,
-                          local_save_path=save_archive_path,
-                          progress_cb=progress_cb)
+        progress_cb = get_progress_cb(
+            api=api,
+            task_id=task_id,
+            message=f"Downloading {remote_path.lstrip('/')}",
+            total=sizeb,
+            is_size=True,
+        )
+        api.file.download(
+            team_id=g.TEAM_ID,
+            remote_path=remote_path,
+            local_save_path=save_archive_path,
+            progress_cb=progress_cb,
+        )
         shutil.unpack_archive(save_archive_path, save_path)
         silent_remove(save_archive_path)
         if len(os.listdir(save_path)) > 1:
             g.my_app.logger.error(
-                "There must be only 1 project directory in the archive")
-            raise Exception(
-                "There must be only 1 project directory in the archive")
+                "There must be only 1 project directory in the archive"
+            )
+            raise Exception("There must be only 1 project directory in the archive")
 
         project_name = os.listdir(save_path)[0]
         project_path = os.path.join(save_path, project_name)
     return project_path
-
-
-def get_dcm_image_name(image_path: str) -> str:
-    """Returns image name with .nrrd extention."""
-    # to save original name of dcm file without ext
-    image_name_with_ext, image_ext = get_file_name_with_ext(
-        image_path), get_file_ext(image_path)
-    if image_ext.lstrip('.').isnumeric():
-        image_name = image_name_with_ext + ".nrrd"
-    else:
-        image_name = get_file_name(image_path) + ".nrrd"
-    return image_name
 
 
 def create_group_tag(group_tag_info: Dict[str, str]) -> sly.Tag:
@@ -120,13 +117,12 @@ def create_group_tag(group_tag_info: Dict[str, str]) -> sly.Tag:
     if group_tag_meta is not None:
         group_tag = sly.Tag(group_tag_meta, group_tag_value)
     else:
-        group_tag_meta = sly.TagMeta(
-            group_tag_name, sly.TagValueType.ANY_STRING)
+        group_tag_meta = sly.TagMeta(group_tag_name, sly.TagValueType.ANY_STRING)
         g.project_meta = g.project_meta.add_tag_meta(group_tag_meta)
-        g.api.project.update_meta(
-            id=g.project_id, meta=g.project_meta.to_json())
+        g.api.project.update_meta(id=g.project_id, meta=g.project_meta.to_json())
         g.api.project.images_grouping(
-            id=g.project_id, enable=True, tag_name=g.GROUP_TAG_NAME)
+            id=g.project_id, enable=True, tag_name=g.GROUP_TAG_NAME
+        )
         group_tag = sly.Tag(group_tag_meta, group_tag_value)
     return group_tag
 
@@ -140,7 +136,9 @@ def create_dcm_tags(dcm: FileDataset) -> List[sly.Tag]:
             dcm_tag_value = str(dcm[dcm_tag].value)
         except:
             dcm_filename = get_file_name_with_ext(dcm.filename)
-            g.my_app.logger.warn(f"Couldn't find key: '{dcm_tag}' in file's metadata: '{dcm_filename}'")
+            g.my_app.logger.warn(
+                f"Couldn't find key: '{dcm_tag}' in file's metadata: '{dcm_filename}'"
+            )
             continue
         if dcm_tag_value is None:
             continue
@@ -148,17 +146,17 @@ def create_dcm_tags(dcm: FileDataset) -> List[sly.Tag]:
         if dcm_tag_meta is not None:
             dcm_tag = sly.Tag(dcm_tag_meta, dcm_tag_value)
         else:
-            dcm_tag_meta = sly.TagMeta(
-                dcm_tag_name, sly.TagValueType.ANY_STRING)
+            dcm_tag_meta = sly.TagMeta(dcm_tag_name, sly.TagValueType.ANY_STRING)
             g.project_meta = g.project_meta.add_tag_meta(dcm_tag_meta)
-            g.api.project.update_meta(
-                id=g.project_id, meta=g.project_meta.to_json())
+            g.api.project.update_meta(id=g.project_id, meta=g.project_meta.to_json())
             dcm_tag = sly.Tag(dcm_tag_meta, dcm_tag_value)
         dcm_tags.append(dcm_tag)
     return dcm_tags
 
 
-def create_ann_with_tags(path_to_img: str, group_tag_info: dict, dcm_tags: List[sly.Tag] = None) -> sly.Annotation:
+def create_ann_with_tags(
+    path_to_img: str, group_tag_info: dict, dcm_tags: List[sly.Tag] = None
+) -> sly.Annotation:
     """Creates annotation with tags."""
     group_tag = create_group_tag(group_tag_info)
     tags_to_add = [group_tag]
@@ -184,7 +182,7 @@ def dcm2nrrd(image_path: str, group_tag_name: str) -> Tuple[str, str, sly.Annota
     pixel_data = dcm.pixel_array
     pixel_data = sly.image.rotate(img=pixel_data, degrees_angle=270)
     original_name = get_file_name_with_ext(image_path)
-    image_name = get_dcm_image_name(image_path)
+    image_name = f"{original_name}.nrrd"
     save_path = os.path.join(os.path.dirname(image_path), image_name)
     nrrd.write(save_path, pixel_data)
 
@@ -194,7 +192,8 @@ def dcm2nrrd(image_path: str, group_tag_name: str) -> Tuple[str, str, sly.Annota
         ann = create_ann_with_tags(save_path, group_tag, dcm_tags)
     except:
         g.my_app.logger.warn(
-            f"Couldn't find key: '{group_tag_name}' in file's metadata: '{original_name}'")
+            f"Couldn't find key: '{group_tag_name}' in file's metadata: '{original_name}'"
+        )
         img_size = nrrd.read_header(save_path)["sizes"].tolist()[::-1]
         ann = sly.Annotation(img_size=img_size)
         if g.ADD_DCM_TAGS:
