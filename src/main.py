04fd2ad5fe20522ba5e02b0ff4b0680db7d68ee8
cxnt <pavelbartsits@gmail.com>
2022-11-30 14:25:10 +0400

    fix nrrd names

--- src/main.py
+++ src/main.py
@@ -1,38 +1,51 @@
 import os
+
 import supervisely as sly
+from supervisely.io.fs import file_exists
 
 import sly_globals as g
 import sly_utils
 
-from supervisely.io.fs import file_exists
-
 
 @g.my_app.callback("import-dicom-studies")
 @sly.timeit
-def import_images_groups(api: sly.Api, task_id: int, context: dict, state: dict, app_logger) -> None:
+def import_dicom_studies(
+    api: sly.Api, task_id: int, context: dict, state: dict, app_logger
+) -> None:
     """Converts DICOM data to .nrrd format and add tags from DICOM metadata."""
     project_dir = sly_utils.download_data_from_team_files(
-        api=api, task_id=task_id, save_path=g.STORAGE_DIR)
+        api=api, task_id=task_id, save_path=g.STORAGE_DIR
+    )
     project_name = os.path.basename(os.path.normpath(project_dir))
     new_project = api.project.create(
-        workspace_id=g.WORKSPACE_ID, name=project_name, change_name_if_conflict=True)
+        workspace_id=g.WORKSPACE_ID, name=project_name, change_name_if_conflict=True
+    )
     g.project_id = new_project.id
 
-    datasets_paths = [os.path.join(project_dir, item) for item in os.listdir(
-        project_dir) if os.path.isdir(os.path.join(project_dir, item))]
+    datasets_paths = [
+        os.path.join(project_dir, item)
+        for item in os.listdir(project_dir)
+        if os.path.isdir(os.path.join(project_dir, item))
+    ]
 
-    ds_progress = sly.Progress(message="Importing Datasets",
-                               total_cnt=len(datasets_paths))
+    ds_progress = sly.Progress(
+        message="Importing Datasets", total_cnt=len(datasets_paths)
+    )
     for dataset_path in datasets_paths:
         dataset_name = os.path.basename(os.path.normpath(dataset_path))
         dataset = api.dataset.create(
-            project_id=new_project.id, name=dataset_name, change_name_if_conflict=True)
-        ds_images_paths = [os.path.join(dataset_path, item) for item in os.listdir(dataset_path) if
-                           file_exists(os.path.join(dataset_path, item)) and sly_utils.is_dicom_file(
-                               os.path.join(dataset_path, item))]
+            project_id=new_project.id, name=dataset_name, change_name_if_conflict=True
+        )
+        ds_images_paths = [
+            os.path.join(dataset_path, item)
+            for item in os.listdir(dataset_path)
+            if file_exists(os.path.join(dataset_path, item))
+            and sly_utils.is_dicom_file(os.path.join(dataset_path, item))
+        ]
 
         batch_progress = sly.Progress(
-            message="Processing images", total_cnt=len(ds_images_paths))
+            message="Processing images", total_cnt=len(ds_images_paths)
+        )
         for batch in sly.batched(ds_images_paths, 50):
             images_paths = []
             images_names = []
@@ -40,13 +53,15 @@ def import_images_groups(api: sly.Api, task_id: int, context: dict, state: dict,
 
             for batch_image_path in batch:
                 image_path, image_name, ann = sly_utils.dcm2nrrd(
-                    image_path=batch_image_path, group_tag_name=g.GROUP_TAG_NAME)
+                    image_path=batch_image_path, group_tag_name=g.GROUP_TAG_NAME
+                )
                 images_paths.append(image_path)
                 images_names.append(image_name)
                 anns.append(ann)
 
             dst_image_infos = api.image.upload_paths(
-                dataset_id=dataset.id, names=images_names, paths=images_paths)
+                dataset_id=dataset.id, names=images_names, paths=images_paths
+            )
             dst_image_ids = [img_info.id for img_info in dst_image_infos]
             api.annotation.upload_anns(img_ids=dst_image_ids, anns=anns)
 
@@ -56,12 +71,11 @@ def import_images_groups(api: sly.Api, task_id: int, context: dict, state: dict,
 
 
 def main():
-    sly.logger.info("Script arguments", extra={
-        "TEAM_ID": g.TEAM_ID,
-        "WORKSPACE_ID": g.WORKSPACE_ID
-    })
+    sly.logger.info(
+        "Script arguments", extra={"TEAM_ID": g.TEAM_ID, "WORKSPACE_ID": g.WORKSPACE_ID}
+    )
     g.my_app.run(initial_events=[{"command": "import-dicom-studies"}])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sly.main_wrapper("main", main)
--- src/sly_globals.py
+++ src/sly_globals.py
@@ -1,34 +1,35 @@
-import os
-import sys
 import json
+import os
+
 import supervisely as sly
+from dotenv import load_dotenv
 from supervisely.app.v1.app_service import AppService
 from supervisely.io.fs import mkdir
 
-app_root_directory = os.path.dirname(os.getcwd())
-sys.path.append(app_root_directory)
-sys.path.append(os.path.join(app_root_directory, "src"))
-print(f"App root directory: {app_root_directory}")
-sly.logger.info(f'PYTHONPATH={os.environ.get("PYTHONPATH", "")}')
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-# order matters
-from dotenv import load_dotenv
-load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
-load_dotenv(os.path.join(app_root_directory, "debug.env"))
 
 my_app: AppService = AppService()
 api: sly.Api = my_app.public_api
 
-TEAM_ID: int = int(os.environ['context.teamId'])
-WORKSPACE_ID: int = int(os.environ['context.workspaceId'])
+TEAM_ID = sly.env.team_id()
+WORKSPACE_ID = sly.env.workspace_id()
 
 TAG_MODE: str = os.environ["modal.state.tagMode"]
-ADD_DCM_TAGS: bool = os.getenv("modal.state.addTagsFromDcm", 'False').lower() in ('true', '1', 't')
+ADD_DCM_TAGS: bool = os.getenv("modal.state.addTagsFromDcm", "False").lower() in (
+    "true",
+    "1",
+    "t",
+)
 
 DCM_TAGS: list = []
 if ADD_DCM_TAGS:
     try:
-        DCM_TAGS: list = json.loads(os.environ["modal.state.dcmTags"].replace('\\', ""))["tags"]
+        DCM_TAGS: list = json.loads(
+            os.environ["modal.state.dcmTags"].replace("\\", "")
+        )["tags"]
     except:
         my_app.logger.warn("Invalid JSON input in modal window editor")
 
