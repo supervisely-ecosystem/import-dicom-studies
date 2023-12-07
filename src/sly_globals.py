6dc9250814e033bd2e3f1033f5c8ceb0359a3b05
almaz <almazgimaev@gmail.com>
2023-12-07 19:43:05 +0100

    check if data is on agent

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -1,8 +1,6 @@
 import json
 import os
-
 from distutils.util import strtobool
-
 
 import supervisely as sly
 from dotenv import load_dotenv
@@ -15,7 +13,7 @@ if sly.is_development():
 
 
 my_app: AppService = AppService()
-# api: sly.Api = my_app.public_api
+api = sly.Api.from_env()
 
 TEAM_ID = sly.env.team_id()
 WORKSPACE_ID = sly.env.workspace_id()
@@ -45,6 +43,11 @@ else:
 
 INPUT_DIR: str = os.environ.get("modal.state.slyFolder")
 INPUT_FILE: str = os.environ.get("modal.state.slyFile")
+
+if INPUT_DIR:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_DIR)
+else:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
 WITH_ANNS: bool = bool(strtobool(os.environ.get("modal.state.withAnns")))
 
